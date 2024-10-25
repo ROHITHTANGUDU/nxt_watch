0b2d04b2a3a2b2396a0a607c2c19f1549cf7fc09
ROHITHTANGUDU <146045295+ROHITHTANGUDU@users.noreply.github.com>
2024-10-25 18:09:13 -0400

    login page created

--- src/Components/Login/index.js
+++ src/Components/Login/index.js
@@ -1,10 +1,29 @@
+import "./index.css"
 const Login = ()=>{
     return(
-        <div>
-            <h1>
-                Login page
-                hello
-            </h1>
+        <div className = "login-back">
+            <div className= "login-card">
+                <img src = "https://assets.ccbp.in/frontend/react-js/nxt-watch-logo-light-theme-img.png" alt = "nxt-logo" className = "nxt-logo"/>
+                <form className = "form">
+                    <label className ="label-element">
+                        USERNAME
+                    </label>
+                    <input type="text"  className = "input-element"/> 
+                    <br/>
+                    <label  className ="label-element">
+                        PASSWORD
+                    </label>
+                    <input type="password"  className="input-element"/> 
+                    <br/>
+                    <div className ="check-box-container">
+                        <input type="checkbox" className = "check-box-input"/>
+                        <label>
+                            Show Password
+                        </label>
+                    </div>
+                </form>
+                <button className = "login-button">Login</button>
+            </div>
         </div>
     )
 }
