a72e11374db5d959cbe8762b8c6889acba6bcd0d
ROHITHTANGUDU <146045295+ROHITHTANGUDU@users.noreply.github.com>
2024-10-25 16:40:07 -0400

    sample

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import {BrowserRouter,Route,Routes} from "react-router-dom";
+import Login from "./Components/Login"
+import Home from "./Components/Home"
+
+
 const App = ()=>{
   return(
-    <div>
-      <h1>
-        hello
-      </h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route excat path = "/login" element={<Login/>}/>
+        <Route excat path = "/home" element={<Home/>}/>
+      </Routes>
+    </BrowserRouter>
+    
   )
 
 }
